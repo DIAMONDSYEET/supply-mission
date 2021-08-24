24fa735c30b759046f448518b5b0b87a6cb0e57f
DIAMONDSYEET <73312696+DIAMONDSYEET@users.noreply.github.com>
2021-08-24 16:23:40 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -81,7 +81,7 @@ function draw() {
  
 }
 
-function keypressed () {
+function keyPressed () {
 	if (keyCode === LEFT_ARROW) {
 		helicopterSprite.x = helicopterSprite.x-20;
 		Matter.body.translate(packageBody, {x:-20,y:0});
